988287664292a1ebb2209110871a48467965e243
Nikita Rusetsky <rusetscky@outlook.com>
2019-07-22 17:50:50 +0800

    Bulma Website Upload

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$('.nav-link, .navbar-brand').click(function() {
-  var sectionTo = $(this).attr('href');
-  $('html, body').animate({
-      scrollTop: $(sectionTo).offset().top - 100
-  }, 500);
-});

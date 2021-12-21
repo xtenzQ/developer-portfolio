31532f157cb0ab156c10286192fdbd491048c605
Nikita Rusetskii <rusetscky@outlook.com>
2021-12-21 23:50:09 +0800

    upload build

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import 'bootstrap/dist/js/bootstrap.bundle'
-import '@fortawesome/fontawesome-free/js/all'
-
-const message = 'Came here to check my code? Go to my GitHub!\nhttps://github.com/xtenzQ!'
-
-console.log(message)

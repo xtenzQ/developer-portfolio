b893710d2cea85561775f549a3e9a63d6756c7dc
Nikita Rusetskii <rusetscky@outlook.com>
2022-03-26 15:36:54 +0800

    add main.js to the end of body tag

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,24 @@ const processMessage = (_0x2f4ax2) => {
 }
 
 console.log(processMessage(7));
+
+AOS.init({disable: 'mobile'});
+
+function calcDate() {
+    const diff = Math.floor(new Date().getTime() - new Date(2020, 12, 10).getTime() + new Date(2020, 1, 4).getTime() - new Date(2020, 1, 2).getTime());
+    const day = 1000 * 60 * 60 * 24;
+
+    const mod_months = Math.floor((diff / day) % 31);
+    const years = Math.floor(diff / day / 31 / 12);
+
+    let year_word = "years";
+    let month_word = "months";
+    if (years === 1) year_word = "year";
+    if (mod_months === 1) month_word = "month";
+
+    const message = years + " " + year_word + " " + mod_months + " " + month_word;
+
+    return message
+}
+
+document.getElementById('time').innerHTML = calcDate();

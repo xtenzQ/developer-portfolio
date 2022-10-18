f7ecc8e2bb13eb4c0e7e8796770cda500f84581f
Nikita Rusetskii <rusetscky@outlook.com>
2022-10-18 18:59:20 +0700

    fix eslint error on serve

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,24 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: "@babel/eslint-parser"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      }
+    ]
+  }
 };

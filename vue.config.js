ec2b26aa45df0d6d187c2ca4b6b15d932be5a6ca
Nikita Rusetskii <rusetscky@outlook.com>
2024-07-28 12:11:05 +0200

    Add comment

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
+    // remove it if you use it at <name>.github.io, otherwise change repo name below
     process.env.NODE_ENV === "production" ? "/developer-portfolio/" : "/",
   css: {
     loaderOptions: {

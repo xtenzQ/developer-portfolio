28d14e69026131385c8cc20a5efd9f23de39a30a
Nikita Rusetskii <rusetscky@outlook.com>
2024-07-28 12:06:47 +0200

    Fix path for vue

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "./",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/developer-portfolio/" : "/",
   css: {
     loaderOptions: {
       sass: {
